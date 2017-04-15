616b49aae5d20698c1c0140e4df588572c6bc7da
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2017-04-14 19:45:53 -0700

    Revert "Simplify lib"
    This reverts commit 5a1750ba7ce7f2b54f1dc6a4affa46cd1e15a9a3.

--- src/lib.rs
+++ src/lib.rs
@@ -70,22 +70,20 @@ impl QLock {
     }
 
     pub fn lock<'r>(&'r self) -> QLockGuard<'r> {
-        LOCAL_NODE_STASH.with(|node_store| unsafe {
-            let mut opt = None;
-            mem::swap(&mut opt, &mut *node_store.borrow_mut());
-            let node = NodeBox::into_raw(opt.unwrap());
+        unsafe {
+            let node = get_local_node();
 
             let head = self.head.swap(node, Ordering::AcqRel);
 
             (*head).wait();
 
-            *node_store.borrow_mut() = Some(NodeBox::from_raw(head));
+            set_local_node(head);
 
             QLockGuard {
                 lock: PhantomData,
                 node: node,
             }
-        })
+        }
     }
 }
 impl<'r> Drop for QLock {
@@ -103,9 +101,18 @@ impl<'r> Drop for QLockGuard<'r> {
         }
     }
 }
-
 thread_local! {
-    static LOCAL_NODE_STASH: RefCell<Option<NodeBox>> = RefCell::new(Some(NodeBox::new()));
+    static VALUE: RefCell<Option<NodeBox>> = RefCell::new(Some(NodeBox::new()));
+}
+
+fn get_local_node() -> *mut QLockNode {
+    let mut value = None;
+    VALUE.with(|x| mem::swap(&mut value, &mut *x.borrow_mut()));
+    return NodeBox::into_raw(value.unwrap());
+}
+
+unsafe fn set_local_node(node: *mut QLockNode) {
+    VALUE.with(|x| *x.borrow_mut() = Some(NodeBox::from_raw(node)));
 }
 
 struct NodeBox {
